8bf5971b26df7fcf835a810495af9852bcddba06
mmjck <msantosjck@gmail.com>
2021-07-08 18:43:00 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App () {
+
+    const [repositories, setRepositories] = useState([]);
+
+    const [location, setLocation] = useState({});
+
+
+
+    useEffect(() => {
+        async function fetchData () {
+            const reponse = await fetch('https://api.github.com/users/mmjck/repos');
+
+            const data = await reponse.json();
+            console.log(data);
+            setRepositories(data);
+        }
+        fetchData();
+    }, []);
+
+
+    useEffect(() => {
+        async function fetchLocation () {
+            const watchId = navigator.geolocation.watchPosition(handlePosition)
+
+            return navigator.geolocation.clearWatch(watchId);
+
+        }
+        fetchLocation();
+    }, []);
+
+    function handlePosition ({ coords }) {
+        console.log('cordenadas', coords);
+
+        const { latitude, longitude } = coords;
+
+        setLocation({ latitude, longitude });
+    }
+
+
+    useEffect(() => {
+        async function isFavorite () {
+            const filtered = repositories.filter(item => item.favorite);
+            document.title = `Você tem ${filtered.length} favorito(s)`
+        }
+        isFavorite();
+    }, [repositories]);
+
+    function handleFavorities (id) {
+        const newRepositories = repositories.map(item => {
+            return item.id === id ? { ...item, favorite: !item.favorite } : item;
+        });
+
+        setRepositories(newRepositories);
+    }
+
+    return (
+        <>
+            <div>
+                <p>Latitude {location.latitude}</p>
+
+                <p>Longitude {location.longitude}</p>
+            </div>
+            <ul>
+                {repositories.map(item => (
+                    <li key={item.id}>
+                        {item.name}
+                        {item.favorite && <span>Favorito</span>}
+                        <button onClick={() => handleFavorities(item.id)}>Favoritar</button>
+                    </li>)
+                )}
+
+            </ul>
+        </>
+
+    );
 }
 
 export default App;
